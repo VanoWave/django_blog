43eb361e985f240b12ebe690c2c6d64e2c5eb50a
student <student@intranet.proweb.uz>
2023-12-02 16:09:33 +0500

    added 123

---
+++ blog/web_site/translation.py
@@ -0,0 +1,19 @@
+from .models import Category, Article
+
+from modeltranslation.translator import TranslationOptions, register
+
+@register(Category)
+class CategoryTranslationOptions(TranslationOptions):
+    fields = (
+        "name",
+    )
+
+
+@register(Article)
+class ArticleTranslationOptions(TranslationOptions):
+    fields = (
+        "title",
+        "short_description",
+        "full_description"
+    )
+
